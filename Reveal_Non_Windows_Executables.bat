0c72a0cf83a373294e7ae3de46d71ce3c4d28801
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2022-01-12 15:06:01 +0000

    Update Reveal_Non_Windows_Executables.bat

--- Reveal_Non_Windows_Executables.bat
+++ Reveal_Non_Windows_Executables.bat
@@ -24,24 +24,27 @@ setlocal EnableDelayedExpansion
 echo:
 
 >nul 2>&1 dism || (
-    echo  To obtain the best results, you should run this script as administrator...
+    echo To obtain the best results, you should run this script as administrator...
     echo:
     start /b mshta vbscript:Execute^("msgbox ""To obtain the best results, you should run this script as administrator..."",69680,""%~nx0"":close"^)
 )
 
-for /f "skip=2tokens=2*delims=," %%A in ('2^>nul wmic process get Description^,ExecutablePath /format:csv') do (
+for /f "skip=1tokens=2*delims=," %%A in ('2^>nul wmic process get Description^,ExecutablePath /format:csv') do (
     set "executable_path=%%~B"
     set "executable_path=!executable_path:~0,-1!"
     if defined executable_path (
+        set "executable_path=!executable_path!"
         if /i not "!executable_path!"=="!memory!" (
             if /i not "!executable_path:~0,11!"=="%SystemRoot%\" (
-                set "Display_Padding=%%~nxA"
-                if "!Display_Padding:~0,20!"=="!Display_Padding!" (
-                    set "Display_Padding=!Display_Padding!                    "
-                    set "Display_Padding=!Display_Padding:~0,20!"
+                if /i not "!executable_path:~0,15!"=="\\?\%SystemRoot%\" (
+                    set "Display_Padding=%%~nxA"
+                    if "!Display_Padding:~0,20!"=="!Display_Padding!" (
+                        set "Display_Padding=!Display_Padding!                    "
+                        set "Display_Padding=!Display_Padding:~0,20!"
+                    )
+                    set "memory=!executable_path!"
+                    echo !Display_Padding!  ^|  !executable_path!
                 )
-                echo PROCESS: !Display_Padding!  ^|  PATH: !executable_path!
-                set "memory=!executable_path!"
             )
         )
     )
